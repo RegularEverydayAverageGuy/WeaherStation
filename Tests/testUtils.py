6c51a20dc2d88908f742a7eabc1f4a4dbdd11a44
RegularEverydayAverageGuy <hipotalamus155@gmail.com>
2023-03-12 15:45:49 +0100

    Added a pylint check and automatic unit test run on changed .py files. The event is triggered on a pre-commit hook.Also a .pylintrc configuration file was added which stores the configuration of pylint

--- Tests/testUtils.py
+++ Tests/testUtils.py
@@ -10,24 +10,30 @@ from utils import utils
 
 #Tests the utils module
 class TestUtils(unittest.TestCase):
-    
+    '''Tests functions from the utils module'''
     #Tests the FormFilesURLs from utils module
-    def test_FilesURL(self):
-        resultingURLs = utils.FormFileURLs("https://test.gov.si/", [])
+    def testFilesURL(self):
+        '''Testing formed URLs which dont have the filenames passed'''
+        resultingURLs = utils.formFileURLs("https://test.gov.si/", [])
         self.assertFalse(resultingURLs)
      #Test resulting file URLs list size
-    def test_FilesURLSize(self):
-        resultingURLs = utils.FormFileURLs("https://test.gov.si/", ["filename"])
+    def testFilesURLSize(self):
+        '''Testing formed URLs size'''
+        resultingURLs = utils.formFileURLs("https://test.gov.si/", ["filename"])
         self.assertTrue(len(resultingURLs) == 1)
     #Test the resulting file URLs validity
-    def test_FilesURLValidity(self):
-        resultingURLs = utils.FormFileURLs("https://test.gov.si/", ["testFilename"])
+    def testFilesURLValidity(self):
+        '''Testing formed if the formed URLs are formed properly'''
+        resultingURLs = utils.formFileURLs("https://test.gov.si/", ["testFilename"])
         self.assertTrue("https://test.gov.si/testFilename" == resultingURLs[0])
     #Test DownloadFiles function with no URL passed as argument
-    def test_DownloadFilesNoURL(self):
-        success = utils.DownloadFiles([], [])
+    def testDownloadFilesNoURL(self):
+        '''Testing if download and storing of files fails if not valid arguments are given'''
+        success = utils.downloadFiles([], [])
         self.assertFalse(success)
      #Test DownloadFiles function with no filenames passed as argument
-    def test_DownloadFilesNoFilenames(self):
-        success = utils.DownloadFiles(["https://test.gov.si/testfilename"], [])
+    def testDownloadFilesNoFilenames(self):
+        '''Testing formed URL which dont have corresponding filnemanes to store'''
+        success = utils.downloadFiles(["https://test.gov.si/testfilename"], [])
         self.assertFalse(success)
+        
--- utils/utils.py
+++ utils/utils.py
@@ -9,25 +9,24 @@ import requests
 from  tqdm import tqdm
 
 #Forms complete file URLs from rootURL and filenames
-def FormFileURLs(rootURL, files):
+def formFileURLs(rootURL, files):
+    '''Function form file URLs'''
     fileURLs = []
     for i in tqdm(range(0, len(files)), desc = "Forming URLs"):
         fileURLs.append(rootURL + files[i])
-        
     return fileURLs
 
-
 #Downloads files from fileURLs list and stores in correspodning file named in fileNames list
-def DownloadFiles(fileURLs, fileNames):
+def downloadFiles(fileURLs, fileNames):
+    '''Function downloading and storing files'''
     #both list have to have same size to properly store data
     if(len(fileNames) != len(fileURLs) or fileURLs == []):
         return False
-    
     #Download and store the data
     for i in tqdm(range(0, len(fileURLs)), desc = "Downloading and storig data"):
-        response = requests.get(fileURLs[i])
+        response = requests.get(fileURLs[i], timeout=5)#Po 5s če ni odgovora prenehamo z zahtevo
         filename = "./Data/" + fileNames[i]
         os.makedirs(os.path.dirname(filename), exist_ok=True)
-        open(filename, "wb").write(response.content)
-    
+        with open(filename, "wb") as file:
+            file.write(response.content)
     return True
