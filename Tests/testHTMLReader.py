1865cb0269128bba5de0994c5efd93b78b3578e4
RegularEverydayAverageGuy <hipotalamus155@gmail.com>
2023-03-19 11:23:05 +0100

    Added unit test for the htmlReader.py module

---
+++ Tests/testHTMLReader.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 19:25:49 2023
+
+@author: dalib
+"""
+
+import unittest
+from unittest.mock import patch, mock_open
+from DataReaders import htmlReader
+
+class TestXMLReader(unittest.TestCase):
+    '''Test class for testing the XMLReader functionality'''
+    @classmethod
+    def setUpClass(cls):
+        cls.reader = htmlReader.HTMLReader("C:/Data/test.html")
+        cls.mockFile = mock_open(read_data="<html><title>Data</title></html>")
+     
+    def testFilenameSetter(self):
+        '''Tests whether the filename is set correctly'''
+        self.reader.setFilename("C:/Data/newTest.html")
+        self.assertEqual(self.reader.filename, "C:/Data/newTest.html")
+        
+    def testWrongFormat(self):
+        '''Tests whether the HTMLReader rejects a wrong file format'''
+        self.reader = htmlReader.HTMLReader("C:/Data/test.xml")
+        success = self.reader.readFile()
+        self.assertFalse(success)      
+        
+        self.reader = htmlReader.HTMLReader("C:/Data/test.txt")
+        success = self.reader.readFile()
+        self.assertFalse(success) 
+   
+    def testCorrectFormat(self):
+        '''Tests whether the function properly reads a file in correct format'''
+        self.reader = htmlReader.HTMLReader("C:/Data/test.html")
+        with patch("builtins.open", self.mockFile):
+            success = self.reader.readFile()
+        
+        self.assertTrue(success)
+        self.assertEqual(self.reader.contents.title.text,"Data")
+        
+    def testEmptyRetrivedData(self):
+        '''Tests whether the retrived data given the input identifier is empty'''
+        with patch("builtins.open", self.mockFile):
+            success = self.reader.readFile()
+        self.assertTrue(success)
+        
+        if success:
+            data = self.reader.getData("")
+            self.assertEqual(data, [])
+    
+    def testNonEmptyRetrivedData(self):
+        '''Tests whether the retrived data given the input identifier is correct'''
+        self.reader = htmlReader.HTMLReader("C:/Data/test.html")
+        with patch("builtins.open", self.mockFile):
+            success = self.reader.readFile()
+        self.assertTrue(success)
+        
+        if success:
+            data = self.reader.getData("title")
+            self.assertEqual(data, ["Data"])
--- Tests/testUtils.py
+++ Tests/testUtils.py
@@ -11,29 +11,35 @@ from utils import utils
 #Tests the utils module
 class TestUtils(unittest.TestCase):
     '''Tests functions from the utils module'''
-    #Tests the FormFilesURLs from utils module
+    
     def testFilesURL(self):
-        '''Testing formed URLs which dont have the filenames passed'''
+        '''Testsformed URLs which dont have the filenames passed'''
         resultingURLs = utils.formFileURLs("https://test.gov.si/", [])
         self.assertFalse(resultingURLs)
-     #Test resulting file URLs list size
+        
     def testFilesURLSize(self):
-        '''Testing formed URLs size'''
+        '''Tests formed URLs size'''
         resultingURLs = utils.formFileURLs("https://test.gov.si/", ["filename"])
         self.assertTrue(len(resultingURLs) == 1)
-    #Test the resulting file URLs validity
+        
     def testFilesURLValidity(self):
-        '''Testing formed if the formed URLs are formed properly'''
+        '''Tests formed if the formed URLs are formed properly'''
         resultingURLs = utils.formFileURLs("https://test.gov.si/", ["testFilename"])
         self.assertTrue("https://test.gov.si/testFilename" == resultingURLs[0])
-    #Test DownloadFiles function with no URL passed as argument
+        
     def testDownloadFilesNoURL(self):
-        '''Testing if download and storing of files fails if not valid arguments are given'''
+        '''Tests if download and storing of files fails if not valid arguments are given'''
         success = utils.downloadFiles([], [])
         self.assertFalse(success)
-     #Test DownloadFiles function with no filenames passed as argument
+        
     def testDownloadFilesNoFilenames(self):
-        '''Testing formed URL which dont have corresponding filnemanes to store'''
+        '''Tests formed URL which dont have corresponding filnemanes to store'''
         success = utils.downloadFiles(["https://test.gov.si/testfilename"], [])
         self.assertFalse(success)
-        
+    
+    def testValidURL(self):
+        '''Test if valid URLs store properly store downloaded data'''
+        url = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/"
+        filename = "observation_CELJE_history.html"
+        success = utils.downloadFiles([url+filename], [filename])
+        self.assertTrue(success)
