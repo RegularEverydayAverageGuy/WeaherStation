4efce3627db1d87ed1a5f5221d7a6046685617ba
RegularEverydayAverageGuy <hipotalamus155@gmail.com>
2023-03-30 20:06:50 +0200

    Changed the project structure and added a cli.py file for easier pacakaging

---
+++ WeatherStation/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 21:47:00 2023
+
+@author: dalib
+"""
+
---
+++ WeatherStation/cli.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 22:17:17 2023
+
+@author: dalib
+"""
+
