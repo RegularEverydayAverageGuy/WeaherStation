53d78fe3554ca307d8fcf9ab9638d9743e2bc52d
RegularEverydayAverageGuy <hipotalamus155@gmail.com>
2023-04-01 16:30:09 +0200

    Added a main function to the cli.py file which run the WeatherStationApp

--- WeatherStation/cli.py
+++ WeatherStation/cli.py
@@ -4,4 +4,14 @@ Created on Wed Mar 29 22:17:17 2023
 
 @author: dalib
 """
+import sys
+from PySide6.QtWidgets import QApplication
+from App import WeatherStationApp
 
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = WeatherStationApp.MainWindow()
+    w.show()
+    app.exec()
+    
