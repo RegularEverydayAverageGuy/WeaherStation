cd12daa40fac44437bc75a638e706cee616d5862
RegularEverydayAverageGuy <110924716+RegularEverydayAverageGuy@users.noreply.github.com>
2023-04-05 21:02:33 +0200

    Added long_description to the setup.py file

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ Created on Thu Mar 30 20:24:24 2023
 @author: dalib
 """
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 with open("requirements.txt", 'r') as f:
     required = f.read().splitlines()
@@ -13,8 +17,9 @@ setup(
     name="WeatherStationApp",
     version="0.0.6",
     description="Display data from https://meteo.arso.gov.si/met/en/service2/",
+    long_description=long_description,
     url="https://github.com/RegularEverydayAverageGuy/WeatherStation",
-    author="Dalibor Maljurić",
+    author="Dalibor Maljuric",
     author_email="dalibor.maljuric@gmail.com",
     license="MIT",
     packages = find_packages(),
